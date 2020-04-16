c279ad19552c7d171bde11bac9a3f93d2a4a5313
Daniel Lopes <viana.daniel02@gmail.com>
2020-04-16 20:01:48 -0300

    Listando todos os carros do banco de dados;

---
+++ src/main/java/com/carros/api/CarrosController.java
@@ -0,0 +1,22 @@
+package com.carros.api;
+
+import com.carros.domain.Carro;
+import com.carros.domain.CarroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/carros")
+public class CarrosController {
+
+    @Autowired
+    private CarroService carroService;
+
+    @GetMapping
+    public Iterable<Carro> get(){
+        return carroService.getCarros();
+    }
+
+}
--- src/main/java/com/carros/domain/CarroService.java
+++ src/main/java/com/carros/domain/CarroService.java
@@ -1,4 +1,17 @@
 package com.carros.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CarroService {
+
+    @Autowired
+    private CarroRepository repository;
+
+    public Iterable<Carro> getCarros(){
+        return repository.findAll();
+    }
+
+
 }
